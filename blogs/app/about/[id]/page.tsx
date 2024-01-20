b61fca7a26d6746c2e6c98045e0fec76499ea56b
mujeebwp <107397056+mujeebwp@users.noreply.github.com>
2024-01-21 02:19:05 +0400

    new bug fixed

---
+++ blogs/app/about/[id]/page.tsx
@@ -0,0 +1,15 @@
+
+import Header from "@/components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+<Header/>
+
+      
+     <h1 className=" text-5xl font-bold">  Hello About us page under   </h1>
+    </main>
+  );
+}
---
+++ blogs/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Counter from "@/components/Counter";
+import Header from "@/components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+<Header/>
+
+      
+     <h1 className=" text-5xl font-bold">  Hello About us page  </h1>
+    </main>
+  );
+}
